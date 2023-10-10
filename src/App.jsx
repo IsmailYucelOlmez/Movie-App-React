502aa166927eb2b8c0e9655205e98ab4829686db
İsmail Yücel Ölmez <ismailyucelolmez514@gmail.com>
2023-10-10 12:47:07 +0300

    Route Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState,useEffect } from 'react'
 import { fetchData } from './api/Api'
 import './App.css'
-//import { useSelector} from 'react-redux/es/hooks/useSelector'
 import { useDispatch,useSelector } from 'react-redux'
 import { getApiConfiguration } from './store/homeSlice'
 import { Routes,Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ExplorePage from './pages/ExplorePage'
+import DetailsPage from './pages/DetailsPage'
+import NotFound from './pages/NotFound'
+import SearchResult from './pages/SearchResult'
+
 
 function App() {
 
@@ -30,12 +35,12 @@ function App() {
       
       <Routes>
 
-        <Route path='' element={} />
-        <Route path='' element={} />
-        <Route path='' element={} />
-        <Route path='' element={} />
-        <Route path='' element={} />
-        
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/:mediaType/:id' element={<DetailsPage/>} />
+        <Route path='/explore/:mediaType' element={<ExplorePage/>} />
+        <Route path='/search/:query' element={<SearchResult/>} />
+        <Route path='*' element={<NotFound/>} />
+
       </Routes>
     </>
   )
