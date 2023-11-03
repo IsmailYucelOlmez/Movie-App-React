0211386909563184548ea1d8ed4b0438432bcb80
İsmail Yücel Ölmez <ismailyucelolmez514@gmail.com>
2023-11-03 18:26:54 +0300

    Not Found Completed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 const NotFound = () => {
 
@@ -7,7 +8,12 @@ const NotFound = () => {
   },[])
   
   return (
-    <div>
+    <div className='lg:container h-112'>
+
+      <h1 className='text-center text-2xl my-5'>Page Not Found</h1>
+
+      <div className='xs:1/3 lg:w-1/4 h-96  mx-auto bg-noresult bg-cover mb-10'>
+      </div>
       
     </div>
   )
